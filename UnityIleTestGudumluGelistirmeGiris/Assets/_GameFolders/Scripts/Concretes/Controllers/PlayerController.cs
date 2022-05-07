cf34ee27a85a2a861507f3a28cf60422fa579015
berkterek <42406263+berkterek@users.noreply.github.com>
2022-05-07 10:47:31 +0300

    feat: NSubstitute dll added and set to edit and play mode test dll

---
+++ UnityIleTestGudumluGelistirmeGiris/Assets/_GameFolders/Scripts/Concretes/Controllers/PlayerController.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace UnityTddBeginner.Controllers
+{
+    public class PlayerController : MonoBehaviour
+    {
+        //Movement
+        //Flip
+        //Jump or Double Jump
+        //Attack
+        //Health
+        //Collect
+        //Animation
+    }    
+}
